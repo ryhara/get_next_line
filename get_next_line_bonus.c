3b9c864b561a799732da587c749206ea0cf9b989
Ryosei Hara <ryhara@c1r22s8.42tokyo.jp>
2023-06-17 12:15:52 +0900

    [Pass] [Fix] #includ _bonus

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 static char	*ft_read_line(int fd, char *buf, char **save)
 {
